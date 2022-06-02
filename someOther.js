a89d0fac279432f3e1f706cadcaf8acabfa1a16a
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-06-02 10:20:59 +0430

    i have learned many thing now but i will

--- someOther.js
+++ someOther.js
@@ -1,6 +1,11 @@
+
 const http = require("http");
-const server=http.createServer();
-server.on("request", (req, res) => {
-    res.end("some data");
+const server = http.createServer((req, res) => {
+    console.log("some we do");
+    res.writeHead(200, { "content-type": "text/plan" });
+    res.write("<h1>html send header</h1>");
+    res.end()
 });
-server.listen(500,console.log("serveris list"));
+
+
+server.listen(500,console.log("serveris listen "));
