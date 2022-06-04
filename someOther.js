e8b01eb6e533b5dea3b8547e833cd88f00e48093
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-06-04 10:39:28 +0430

    I strted express

--- someOther.js
+++ someOther.js
@@ -1,10 +1,22 @@
+const { readFileSync } = require("fs");
+const page = readFileSync("./navbar-app/index.html", "utf-8");
+const pageStyle = readFileSync("./navbar-app/styles.css", "utf-8");
 
 const http = require("http");
+
 const server = http.createServer((req, res) => {
-    console.log("some we do");
-    res.writeHead(200, { "content-type": "text/plan" });
-    res.write("<h1>html send header</h1>");
-    res.end()
+    const url = req.url;
+    console.log(url);
+    if (url === "/") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write(page);
+        res.end();
+    }
+    else if (url === "/styles.css") {
+        res.writeHead(200, { "content-type": "text/css" });
+        res.write(pageStyle);
+        res.end();
+    }
 });
 
 
